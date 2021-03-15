e7b0d1981fae2cccfe85c43288ab12d6dd212a50
team mindset.io <team@mindset.io>
2021-03-15 09:33:16 -0700

    initial commit

---
+++ log_stream.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import multiprocessing as mp
+import re
+import time
+from os import environ
+from pathlib import Path
+from urllib.error import HTTPError
+from urllib.request import Request, urlopen
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(level=environ.get("LOGLEVEL", "INFO"))
+
+
+class LogStream:
+    API_URL = f"api.mindset.io/apps/logtrack/run?api_key={environ['API_KEY']}"
+    HEADERS = {
+        "Content-Type": "application/json; charset=utf-8",
+        "User-Agent": "Syslog stream",
+    }
+
+    def __init__(self, rec_type, logfile, regex):
+        self.rec_type = rec_type
+        self.logfile = Path(logfile)
+        self.regex = [regex] if isinstance(regex, str) else regex
+        log.info(f"listening on {self.logfile}")
+
+    @staticmethod
+    def tail(fh):
+        fh.seek(0, 2)
+        while True:
+            line = fh.readline()
+            if line:
+                yield line
+            else:
+                time.sleep(0.1)
+
+    def post_record(self, record):
+        data = json.dumps(
+            {
+                "data": {
+                    "record": {
+                        "type": self.rec_type,
+                        "content": record,
+                    }
+                }
+            }
+        )
+        log.info(f"posting log record ({self.rec_type}): {record}")
+        req = Request(
+            f"https://{self.API_URL}",
+            data=data.encode("utf8"),
+            method="POST",
+            headers=self.HEADERS,
+        )
+        try:
+            _ = urlopen(req)
+        except HTTPError as e:
+            log.error(f"HTTP ERROR: {e.msg} ({e.code})")
+            log.error(f"{e.read().decode()}")
+
+    def _matches(self, re_str):
+        while True:
+            record = (yield).strip()
+            if re.findall(re_str, record):
+                self.post_record(record)
+
+    def stream(self):
+        matches = [self._matches(smatch) for smatch in self.regex]
+        _ = [next(m) for m in matches]
+
+        while True:
+            fh = open(self.logfile)
+            for line in self.tail(fh):
+                for m in matches:
+                    m.send(line)
+
+
+if __name__ == "__main__":
+    with open(Path("config.json")) as f:
+        config = json.load(f)
+    jobs = [
+        mp.Process(target=LogStream(stype, **params).stream, args=())
+        for stype, params in config.items()
+    ]
+    _ = [j.start() for j in jobs]
---
+++ log_tracking_app.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+from collections import deque
+from datetime import datetime
+from functools import wraps
+import json
+import os
+
+import geoip2.database
+from jinja2 import Environment, FileSystemLoader
+
+
+GEO_DB_PATH = "/data/GeoIP2/GeoLite2-City.mmdb"
+TEMPLATE_DIR = os.path.dirname(os.path.realpath(__file__))
+TEMPLATE_ENV = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+TEMPLATE = TEMPLATE_ENV.get_template("template.html")
+with open("world.json") as fh:
+    WORLD_JSON = fh.read()
+
+
+def format_timestamp(mo, day, hhmm):
+    _ts = f"{datetime.today().year} {mo} {int(day):2d} {hhmm}"
+    timestamp = datetime.strptime(_ts, "%Y %b %d %X")
+    return timestamp
+
+
+def record_parse(content, split_with):
+    meta, rec = content.split(split_with)
+    mo, day, hhmm, host, *_ = [s for s in meta.split(" ") if s]
+    timestamp = format_timestamp(mo, day, hhmm)
+    common = {"host": host, "timestamp": f"{timestamp}"}
+    return common, rec.split(" ")
+
+
+def log_decorator(ip_key):
+    def deco(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            cdct, pdct = func(*args, **kwargs)
+            rec_type = func.__name__.removeprefix("parse_").upper()
+            geoip = geo_data(pdct[ip_key])
+            dct = {**cdct, **pdct, "type": rec_type, "geoip": geoip}
+            return dct
+
+        return wrapper
+
+    return deco
+
+
+@log_decorator("SRC")
+def parse_ufw_block(content):
+    common_dct, rec = record_parse(content, " [UFW BLOCK] ")
+    parsed_dct = {
+        ss[0]: ss[1] or None for s in rec if len(ss := s.split("=")) == 2
+    }
+    return common_dct, parsed_dct
+
+
+@log_decorator("SRC")
+def parse_ssh_invalid(content):
+    for split_by, record_type, idxs in [
+        (": Invalid user ", "SSH_INVALID", [4, 2, 0]),
+        (": Connection closed by authenticating user ", "SSH_AUTH", [3, 1, 0]),
+    ]:
+        try:
+            common_dct, rec = record_parse(content, split_by)
+            parsed_dct = {
+                "DPT": rec[idxs[0]],
+                "SRC": rec[idxs[1]],
+                "USERNAME": rec[idxs[2]],
+            }
+            return common_dct, parsed_dct
+        except ValueError:
+            pass
+
+
+def geo_data(ipaddr):
+    with geoip2.database.Reader(GEO_DB_PATH) as reader:
+        city = reader.city(ipaddr)
+    dct = {
+        "iso_code": city.country.iso_code,
+        "country": city.country.name,
+        "subdivision": city.subdivisions.most_specific.iso_code,
+        "city": city.city.name,
+        "coords": {
+            "lat": city.location.latitude,
+            "lon": city.location.longitude,
+        },
+    }
+    return dct
+
+
+def make_page(history, tablelen):
+    params = {
+        "data": history,
+        "tablelen": tablelen,
+        "world": WORLD_JSON,
+        "username": os.environ.get("MSIO_USERNAME"),
+        "app_idn": os.environ.get("MSIO_APP_ALIAS")
+        or os.environ.get("MSIO_APP_ID"),
+    }
+    html = TEMPLATE.render(**params)
+    return html
+
+
+def main(record, history=None, tablelen=10):
+    func_name = f"parse_{record['type'].lower()}"
+    rec = globals()[func_name](record["content"])
+    html = make_page((history or []) + [rec], tablelen)
+    return {"record": rec, "html": html}
+
+
+if __name__ == "__main__":  # Local testing
+
+    def stream_data():
+        with open("sample.jsonl") as f:
+            for line in f.readlines():
+                if line := line.strip():
+                    yield json.loads(line)
+
+    os.environ["MSIO_USERNAME"] = "local-user"
+    os.environ["MSIO_APP_ALIAS"] = "log-tracker"
+    history, tablelen = [], 15
+    for record in stream_data():
+        resp = main(record, history, tablelen=tablelen)
+        history.append(resp["record"])
+
+    with open(f"/tmp/page.html", "w") as f:
+        f.write(resp["html"])
